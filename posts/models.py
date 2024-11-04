bec08b3653742c52905e4edee40eba7766ea5216
r0manch1k <romansokolv209@gmail.com>
2024-11-04 21:20:43 +1000

    add models in users

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+# use URLField for url to project or any object in post
+# use ForeignKey to associate a post with its author
+# use ManyToManyField to associate a post with its collections, tags (NOT category)
+# FileField for loading pdfs for example
--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+# Should we put Group of Users here? (e.g. Students, Teachers, System Programming Student etc.)
 
+# Put AUTH_USER_MODEL = 'users.User' in settings.py
+
+class User(AbstractUser):
+    # groups = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, blank=True)
+
+    # Is it better to use WEBP for images? django-resized contains ResizeImageField that works with pillow automatically
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name="Avatar")
+
+    # Points to show activity (yes, it can be negative :0 )
+    points = models.IntegerField(default=0)
+
+    # Awards for User (e.g. "The Holy Ghost - Gain over 50 likes(stars) on Lecture Note")
+    awards = models.ManyToManyField('Award', related_name='awards', blank=True)
+
+    # Contacts
+    telegram = models.URLField(blank=True, null=True, verbose_name="Telegram")
+    vkontakte = models.URLField(blank=True, null=True, verbose_name="VK")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
