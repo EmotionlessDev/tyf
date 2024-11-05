6b5806c82b932fcb967cce2e4d1a44e797086414
r0manch1k <romansokolv209@gmail.com>
2024-11-05 15:24:44 +1000

    change models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as UserAdminDefault
+from django.contrib.auth.forms import UserChangeForm as UserChangeFormDefault
+from django.contrib.auth.forms import UserCreationForm as UserCreationFormDefault
+# from users.forms import CustomUserChangeForm, CustomUserCreationForm
+from users.models import User
 
-# Register your models here.
+
+class UserCreationForm(UserCreationFormDefault):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class UserChangeForm(UserChangeFormDefault):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class UserAdmin(UserAdminDefault):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+    fieldsets = (
+        (None, {'fields':
+                    ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')}
+         ),
+        ('Permissions', {'fields':
+                             ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}
+         ),
+    )
+    add_fieldsets = (
+        (None, {'fields':
+                    ('first_name', 'last_name', 'email', 'password1', 'password2', 'date_of_birth', 'is_staff',
+                     'is_active', 'groups', 'user_permissions')}
+         ),
+    )
+
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(User, UserAdmin)
