f19db6214b38e47a35462a537fe804fc790cf357
r0manch1k <romansokolv209@gmail.com>
2024-11-06 01:42:54 +1000

    add users

---
+++ users/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as UserAdminDefault
+from django.contrib.auth.forms import UserChangeForm as UserChangeFormDefault
+from django.contrib.auth.forms import UserCreationForm as UserCreationFormDefault
