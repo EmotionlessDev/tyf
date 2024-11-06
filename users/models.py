bb864399033ee50ad30811fd686d0a0c59f166eb
EmotionlessDev <emotionlesscode@gmail.com>
2024-11-07 00:42:16 +1000

    fix create useers

--- users/models.py
+++ users/models.py
@@ -4,13 +4,13 @@ from django.db import models
 # Create your models here.
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
         if not password:
             raise ValueError('Users must have a password')
         email = self.normalize_email(email)
-        user = self.model(email=email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.is_active = True
         user.save()
@@ -30,7 +30,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=50, unique=True, blank=True, null=False)
+    username = models.CharField(max_length=50, unique=True, blank=True, null=True)
     email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=False, verbose_name="Avatar")
     points = models.IntegerField(default=0)
@@ -39,6 +39,7 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    objects = UserManager()
 
     def __str__(self):
         return self.username
