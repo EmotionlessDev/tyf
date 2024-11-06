9f3aa92ffa3c838a9ab3fb3441fb25f2a7723f2c
EmotionlessDev <emotionlesscode@gmail.com>
2024-11-06 23:56:17 +1000

    refactor model user

--- users/models.py
+++ users/models.py
@@ -4,12 +4,15 @@ from django.db import models
 # Create your models here.
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password=None):
         if not email:
             raise ValueError('Users must have an email address')
+        if not password:
+            raise ValueError('Users must have a password')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email)
         user.set_password(password)
+        user.is_active = True
         user.save()
         return user
 
@@ -27,39 +30,15 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
-    # groups = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, blank=True)
-
     username = models.CharField(max_length=50, unique=True, blank=True, null=False)
-
     email = models.EmailField(unique=True)
-
-    # Is it better to use WEBP for images? django-resized contains ResizeImageField that works with pillow automatically
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=False, verbose_name="Avatar")
-
-    # Points to show activity (yes, it can be negative :0 )
     points = models.IntegerField(default=0)
-
-    # Awards for User (e.g. "The Holy Ghost - Gain over 50 likes(stars) on Lecture Note")
-    # awards = models.ManyToManyField('Award', related_name='awards', blank=True)
-
-    # Contacts
     telegram = models.URLField(blank=True, null=True, verbose_name="Telegram")
     vkontakte = models.URLField(blank=True, null=True, verbose_name="VK")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-
-    def save(self, *args, **kwargs):
-        if not self.username:
-            self.username = self.generate_username()
-
-        super(User, self).save(*args, **kwargs)
-
-    @staticmethod
-    def generate_username():
-        # We can use this to generate random username:
-        # https://pypi.org/project/random-username/
-        return "Kukold228"
 
     def __str__(self):
         return self.username
