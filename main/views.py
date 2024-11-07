d23de02f5dbcc156c1695a99289a5eb5cb2655a6
EmotionlessDev <emotionlesscode@gmail.com>
2024-11-08 00:46:38 +1000

    added base profile template

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/blank.html')
+
+def profile(request):
+    return render(request, 'main/profile.html')
