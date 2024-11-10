bbe42a9305acd55685e204e5a39e5fe9c1d8341f
EmotionlessDev <emotionlesscode@gmail.com>
2024-11-10 14:43:14 +1000

    profile routes

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from users.models import User
 
 def index(request):
     return render(request, 'main/blank.html')
 
-def profile(request):
-    return render(request, 'main/profile.html')
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    return render(request, 'main/profile.html', {"user": user})
