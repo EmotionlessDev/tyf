290b0ddd42902495f47fcd69b5b65ba9082bbff1
EmotionlessDev <emotionlesscode@gmail.com>
2024-11-10 19:10:25 +1000

    added basic ui

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("posts/", views.posts_list, name="posts_list"),
     path("register/", views.register, name="register"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
--- main/views.py
+++ main/views.py
@@ -60,53 +60,68 @@ def index(request: HttpRequest) -> HttpResponse:
 
 
 def profile(request: HttpRequest, username: str) -> HttpResponse:
-    user = get_object_or_404(User, username=username)
-    return render(request, "main/profile.html", {"user": user})
-
-
-def posts_list(request: HttpRequest) -> HttpResponse:
     cards = [
         {
             "title": "Название работы 1",
             "description": "Конспект по математическому анализу",
             "language": "LaTeX",
             "stars": 5,
+            "author": "Иван Иванов",
+            "date": "2024-10-01",
         },
         {
             "title": "Название работы 2",
             "description": "Теория вероятностей. Введение",
             "language": "Python",
             "stars": 8,
+            "author": "Мария Петрова",
+            "date": "2024-09-15",
         },
         {
             "title": "Название работы 3",
             "description": "Математическая логика",
             "language": "Java",
             "stars": 12,
+            "author": "Алексей Смирнов",
+            "date": "2024-08-20",
         },
         {
             "title": "Название работы 4",
             "description": "Алгоритмы и структуры данных",
             "language": "C++",
             "stars": 15,
+            "author": "Светлана Кузнецова",
+            "date": "2024-07-30",
         },
         {
             "title": "Название работы 5",
             "description": "Модели машинного обучения",
             "language": "R",
             "stars": 7,
+            "author": "Дмитрий Федоров",
+            "date": "2024-06-15",
         },
         {
             "title": "Название работы 6",
             "description": "Курс по сетям и базам данных",
             "language": "PHP",
             "stars": 5,
+            "author": "Елена Волкова",
+            "date": "2024-05-10",
         },
         {
             "title": "Название работы 7",
             "description": "Разработка веб-приложений",
             "language": "JavaScript",
             "stars": 10,
+            "author": "Константин Чернов",
+            "date": "2024-04-25",
         },
     ]
-    return render(request, "main/posts_list.html", {"cards": cards})
+    user = get_object_or_404(User, username=username)
+    return render(request, "main/profile.html", {"user": user, "cards": cards})
+
+
+# def posts_list(request: HttpRequest) -> HttpResponse:
+#
+#     return render(request, "main/posts_list.html", {"cards": cards})
