8bdbfa93f99e82a00b6398638cb6df711e093467
EmotionlessDev <emotionlesscode@gmail.com>
2024-11-13 00:27:03 +1000

    stable fix v

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from .models import Post, Comment
+from django.http import HttpResponse, HttpRequest
 from users.models import User
 from django.shortcuts import get_object_or_404, render
 from django.contrib import messages
@@ -6,13 +8,53 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login as login_user, logout as logout_user
 from django.apps import apps
 from django.views.generic import DetailView
+from .models import Profile
 
+# def post_detail(request, post_id):
+#     post = get_object_or_404(Post, id=post_id)
+#     comments = post.comments.filter(active=True, parent__isnull=True)
+#     if request.method == "POST":
+#         # comment has been added
+#         comment_form = CommentForm(data=request.POST)
+#         if comment_form.is_valid():
+#             parent_obj = None
+#             # get parent comment id from hidden input
+#             try:
+#                 # id integer e.g. 15
+#                 parent_id = int(request.POST.get("parent_id"))
+#             except:
+#                 parent_id = None
+#             # if parent_id has been submitted get parent_obj id
+#             if parent_id:
+#                 parent_obj = Comment.objects.get(id=parent_id)
+#                 # if parent object exist
+#                 if parent_obj:
+#                     # create replay comment object
+#                     replay_comment = comment_form.save(commit=False)
+#                     # assign parent_obj to replay comment
+#                     replay_comment.parent = parent_obj
+#             # normal comment
+#             # create comment object but do not save to database
+#             new_comment = comment_form.save(commit=False)
+#             # assign ship to the comment
+#             new_comment.post = post
+#             # save
+#             new_comment.save()
+#             return HttpResponseRedirect(post.get_absolute_url())
+#     else:
+#         comment_form = CommentForm()
+#     return render(
+#         request,
+#         "core/detail.html",
+#         {"post": post, "comments": comments, "comment_form": comment_form},
+#     )
 
-def index(request):
-    return render(request, "main/blank.html")
+def register(request):
+    form = UserCreationForm()
 
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
 
-def profile(request, username):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("#")
@@ -61,12 +103,8 @@ def index(request: HttpRequest) -> HttpResponse:
 
 def profile(request: HttpRequest, username: str) -> HttpResponse:
     user_posts = Post.objects.filter(author__username=username)
-    user = get_object_or_404(User, username=username)
+    user = get_object_or_404(Profile, username=username)
     return render(request, "main/profile.html", {"user": user, "posts": user_posts})
-
-
-def single_post(request: HttpRequest) -> HttpResponse:
-    return render(request, "main/single-post.html")
 
 
 class PostDetailView(DetailView):
